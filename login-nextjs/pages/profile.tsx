cc27b5e428c1043bce3290da6e731814b9c6f19d
Enguerran <enguerran.huby@aroed.fr>
2022-12-06 17:17:47 +0100

    Start profile

--- login-nextjs/pages/profile.tsx
+++ login-nextjs/pages/profile.tsx
@@ -1,10 +1,37 @@
-import { Heading, Flex } from "@chakra-ui/react";
+import { Heading, Text, Container, Center, VStack, Image, Button } from "@chakra-ui/react";
+import { useSession } from "next-auth/react";
 
 const profile = () => {
+  const { data: session } = useSession();
+
   return (
-    <Flex justifyContent="center">
-      <Heading>Profile</Heading>
-    </Flex>
+
+    <Container mt={4}>
+      <Center>
+        <VStack>
+          <Heading>Profile</Heading>
+          <Image
+            src={
+              session?.user?.image ||
+              'https://images.pexels.com/photos/13243820/pexels-photo-13243820.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+            }
+            alt='picture'
+            boxSize="200px"
+            borderRadius="full"
+            mx="auto"
+            border="1px solid black"
+          />
+          <Button colorScheme='teal' size='md'>
+            Change picture
+          </Button>
+          <Text color="gray" fontSize={30}>Name</Text>
+          <Text color="teal" fontSize={15}>{session?.user?.name}</Text>
+          <Text color="gray" fontSize={30}>Email</Text>
+          <Text color="teal" fontSize={15}>{session?.user?.email}</Text>
+        </VStack>
+      </Center>
+    </Container>
+
   );
 };
 
